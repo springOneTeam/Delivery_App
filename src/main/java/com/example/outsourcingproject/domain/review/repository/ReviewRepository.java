832606e08f6376cea8795f9c9ed484ea3ba0da38
wldnr1208 <wldnr1208@naver.com>
2025-01-08 21:24:18 +0900

    Feat: All Create Done

--- src/main/java/com/example/outsourcingproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/review/repository/ReviewRepository.java
@@ -14,8 +14,12 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         return findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id = " + id));
     }
 
-    List<Review> findByStoreId(Long storeId);
+    List<Review> findByStore_StoreId(Long storeId);
 
-    List<Review> findByStoreIdAndRatingBetween(Long storeId, int minRating, int maxRating);
+    // List<Review> findByStoreIdAndRatingBetween(Long storeId, int minRating, int maxRating);
+
+    // findByStoreIdAndRatingBetween 메서드에서 JPA는 'storeId'를 Store 엔티티의 속성으로 인식하려고 합니다.
+    // 하지만 실제로는 Review 엔티티가 Store 엔티티 자체를 참조하고 있는 상황입니다.
+    List<Review> findByStore_StoreIdAndRatingBetween(Long storeId, int minRating, int maxRating);
 
 }
--- src/main/java/com/example/outsourcingproject/domain/user/utils/JwtUtil.java
+++ src/main/java/com/example/outsourcingproject/domain/user/utils/JwtUtil.java
@@ -1,21 +1,21 @@
-package com.example.outsourcingproject.domain.user.utils;
-
-import javax.crypto.SecretKey;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import io.jsonwebtoken.security.Keys;
-
-@Component
-public class JwtUtil {
-
-	private final SecretKey key;
-	private static final long TOKEN_VALID_TIME = 1000L * 60 * 60;
-
-	public JwtUtil(@Value("{secret-key}") String key) {
-		this.key = Keys.hmacShaKeyFor(key.getBytes());
-	}
-
-
-}
+// package com.example.outsourcingproject.domain.user.utils;
+//
+// import javax.crypto.SecretKey;
+//
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
+//
+// import io.jsonwebtoken.security.Keys;
+//
+// @Component
+// public class JwtUtil {
+//
+// 	private final SecretKey key;
+// 	private static final long TOKEN_VALID_TIME = 1000L * 60 * 60;
+//
+// 	public JwtUtil(@Value("{secret-key}") String key) {
+// 		this.key = Keys.hmacShaKeyFor(key.getBytes());
+// 	}
+//
+//
+// }
