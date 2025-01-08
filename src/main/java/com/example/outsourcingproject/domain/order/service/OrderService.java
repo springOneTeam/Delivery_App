c0342c47e2b2d9ae28e1703da510068fcfd9a40d
임PRO스터\su992 <su9926@naver.com>
2025-01-08 17:46:23 +0900

    Feat: Order Service 추가
    - createOrder 메서드 추가

--- src/main/java/com/example/outsourcingproject/domain/order/service/OrderService.java
+++ src/main/java/com/example/outsourcingproject/domain/order/service/OrderService.java
@@ -1,4 +1,32 @@
 package com.example.outsourcingproject.domain.order.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.outsourcingproject.domain.order.dto.CreateOrderRequestDto;
+import com.example.outsourcingproject.domain.order.entity.Order;
+import com.example.outsourcingproject.domain.order.repository.OrderRepository;
+import com.example.outsourcingproject.domain.store.entity.Store;
+import com.example.outsourcingproject.domain.store.repository.StoreRepository;
+import com.example.outsourcingproject.exception.GlobalExceptionHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+
+	private final OrderRepository orderRepository;
+	private final StoreRepository storeRepository;
+	private final GlobalExceptionHandler globalExceptionHandler;
+
+	public void createOrder(Long userId, CreateOrderRequestDto createOrderRequestDto) {
+		/*Todo 예외처리 수정 예정*/
+		Store store = storeRepository.findById(createOrderRequestDto.storeId())
+			.orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+		/*Todo menu 추가 예정*/
+		Order order = new Order();
+		Order saveOrder=orderRepository.save(order);
+	}
 }
