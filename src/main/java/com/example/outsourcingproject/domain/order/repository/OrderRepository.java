1795c36b39d7838a7d0779d7cdd8cb74d5e80711
임PRO스터\su992 <su9926@naver.com>
2025-01-08 17:47:10 +0900

    Feat: Order Repository 추가
    - extends JpaRepository

--- src/main/java/com/example/outsourcingproject/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/order/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.example.outsourcingproject.domain.order.repository;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.outsourcingproject.domain.order.entity.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
