dca0f71faf225b8c5ca96a623ba397a948d5c581
임PRO스터\su992 <su9926@naver.com>
2025-01-08 17:45:51 +0900

    Feat: Order Controller 추가
    - createOrder PostMapping 추가

--- src/main/java/com/example/outsourcingproject/domain/order/controller/OrderController.java
+++ src/main/java/com/example/outsourcingproject/domain/order/controller/OrderController.java
@@ -1,4 +1,33 @@
 package com.example.outsourcingproject.domain.order.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.outsourcingproject.domain.order.dto.CreateOrderRequestDto;
+import com.example.outsourcingproject.domain.order.dto.CreateOrderResponseDto;
+import com.example.outsourcingproject.domain.order.service.OrderService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+	private final OrderService orderService;
+
+	@PostMapping
+	public ResponseEntity<CreateOrderResponseDto> creatOrder(
+		HttpServletRequest request,
+		@RequestBody CreateOrderRequestDto createOrderRequestDto
+	){
+		Long userId=(Long)request.getAttribute("userId");
+		orderService.createOrder(userId, createOrderRequestDto);
+		return new ResponseEntity<>(new CreateOrderResponseDto("음식을 주문했습니다."), HttpStatus.OK);
+	}
 }
