678c2030086dce45d969e36068c598e5e798039a
wldnr1208 <wldnr1208@naver.com>
2025-01-09 19:23:07 +0900

    Feat: Store get temporary

--- src/main/java/com/example/outsourcingproject/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/store/repository/StoreRepository.java
@@ -1,8 +1,9 @@
 package com.example.outsourcingproject.domain.store.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.outsourcingproject.domain.store.entity.Store;
@@ -11,4 +12,8 @@ import com.example.outsourcingproject.domain.user.entity.User;
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
 	long countByOwner(User owner);
+	// 정확한 가게명으로 단건 조회
+	Optional<Store> findByStoreName(String storeName);
+	List<Store> findAllByStoreName(String storeName);
+
 }
