e1f9a55ce0c51dd1bc994e9cf1e6c7c12d53702a
t2023-m0014 <qa9377@gmail.com>
2025-01-09 11:46:49 +0900

    Feat: 최신순 정렬 추가

--- src/main/java/com/example/outsourcingproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/review/repository/ReviewRepository.java
@@ -14,12 +14,10 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         return findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id = " + id));
     }
 
-    List<Review> findByStore_StoreId(Long storeId);
+    List<Review> findByStoreIdOrderByCreatedAtDesc(Long storeId);
 
     // List<Review> findByStoreIdAndRatingBetween(Long storeId, int minRating, int maxRating);
 
-    // findByStoreIdAndRatingBetween 메서드에서 JPA는 'storeId'를 Store 엔티티의 속성으로 인식하려고 합니다.
-    // 하지만 실제로는 Review 엔티티가 Store 엔티티 자체를 참조하고 있는 상황입니다.
-    List<Review> findByStore_StoreIdAndRatingBetween(Long storeId, int minRating, int maxRating);
+    List<Review> findByStore_StoreIdAndRatingBetweenOrderByCreatedAtDesc(Long storeId, int minRating, int maxRating);
 
 }
