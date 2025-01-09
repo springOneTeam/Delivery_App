d4aa70c5af0f828589d64a4a4536ee1518fa17f3
sinwoo-kim <shinwoo5676@gmail.com>
2025-01-09 23:34:06 +0900

    Feat: Service UserLogin 추가

--- src/main/java/com/example/outsourcingproject/domain/user/service/UserService.java
+++ src/main/java/com/example/outsourcingproject/domain/user/service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.outsourcingproject.domain.user.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.outsourcingproject.domain.user.dto.request.UserLoginRequesetDto;
 import com.example.outsourcingproject.domain.user.dto.request.UserSignUpRequestDto;
+import com.example.outsourcingproject.domain.user.dto.response.UserLoginResponseDto;
 import com.example.outsourcingproject.domain.user.dto.response.UserSignUpResponseDto;
 import com.example.outsourcingproject.domain.user.entity.User;
 import com.example.outsourcingproject.domain.user.repository.UserRepository;
@@ -18,6 +21,7 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final JwtUtil jwtUtil;
+	private final PasswordEncoder passwordEncoder;
 
 	/**
 	 * TODO :: 추후 토큰 생성 후 반환
@@ -27,31 +31,45 @@ public class UserService {
 	public UserSignUpResponseDto signUpUser(UserSignUpRequestDto requestDto) {
 		// 비즈니스 규칙: 이메일 검증
 		verifyEmail(requestDto.email());
-
 		// 비즈니스 규칙: 비밀번호 형식 검증
 		User.validatePassword(requestDto.password());
-
 		// 비밀번호 암호화
 		String encryptedPassword = User.generateEncryptedPassword(requestDto.password());
 
 		// 새 유저 생성
 		User newUser = User.create(requestDto.nickName(), requestDto.email(), encryptedPassword, requestDto.role());
-
 		User savedUser = userRepository.save(newUser);
-
-		String generatedToken = jwtUtil.generateToken(savedUser.getUserId(), savedUser.getRole().toString());
-
-		return UserSignUpResponseDto.toDto(savedUser.getRole(), generatedToken);
+		return UserSignUpResponseDto.toDto(savedUser.getRole());
 	}
 
 
-	//------------------- private common Methods -------------------
-	private void verifyEmail(String Email) {
-		User.generateEmail(Email);
+	public UserLoginResponseDto loginUser(UserLoginRequesetDto requestDto) {
+		User foundUser = userRepository.findByEmail(requestDto.email());
+
+		// 비즈니스 규칙: 이메일이 중복되어서는 안된다.
+		matchPassword(requestDto.password(), foundUser.getPassword());
+		String generatedToken = jwtUtil.generateToken(foundUser.getUserId(), foundUser.getRole().toString());
+		return UserLoginResponseDto.toDto(generatedToken);
 	}
 
+	//------------------- public Methods -------------------
 	public User getUserById(Long userId) {
 		return userRepository.findById(userId)
 			.orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
 	}
+
+	//------------------- private Methods -------------------
+	private void verifyEmail(String Email) {
+		User.generateEmail(Email);
+		if (userRepository.existsByEmail(Email)) {
+			throw new BusinessException(ErrorCode.DUPLICATE_EMAIL);
+		}
+	}
+	// 엔티티로 옮겨야 됨.
+	private void matchPassword(String requestPassword, String storagePassword) {
+		if (!passwordEncoder.matches(requestPassword, storagePassword)) {
+			throw new BusinessException(ErrorCode.LOGIN_FAILED);
+		}
+	}
+
 }
