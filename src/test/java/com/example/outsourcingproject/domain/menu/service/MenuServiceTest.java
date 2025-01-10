b89b0c71bcc838ec6f8a4202c77942de27fbe02d
joacho <joacho1205@gmail.com>
2025-01-10 18:37:25 +0900

    Fix: 이미 삭제된 메뉴는 수정,삭제 추가로 안되도록

--- src/test/java/com/example/outsourcingproject/domain/menu/service/MenuServiceTest.java
+++ src/test/java/com/example/outsourcingproject/domain/menu/service/MenuServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.outsourcingproject.domain.menu.service;
 
-import static java.awt.SystemColor.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -38,6 +37,12 @@ class MenuServiceTest {
 	private static final Long menuId = 1L;
 	private static final Long userId = 1L;
 
+	/**
+	 * 메뉴 생성 성공 테스트
+	 * given: 테스트에 필요한 초기 상태 설정. DTO, Mock객체, 반환값 설정
+	 * when: createMenu 메서드 실행, Mock설정에 따라 storeService.findStoreById, menuRepository.save 호출
+	 * then: 생성된 메뉴의 이름과 가격이 기댓값과 같은지 검증
+	 */
 	@Test
 	@DisplayName("메뉴 생성 성공")
 	public void createMenuTest() {
@@ -66,6 +71,12 @@ class MenuServiceTest {
 		assertThat(10000).isEqualTo(responseDto.price());
 	}
 
+	/**
+	 * 메뉴 수정 성공 테스트
+	 * given: 수정 요청 DTO와 Mock객체의 동작 설정. 메뉴 수정 시 이름과 가격이 올바르게 업데이트되는지 확인하기 위해 doAnswer 사용
+	 * when: updateMenu 메서드 실행. Mock 설정에 따라 storeService.findStoreById, menuRepository.findById 이 호출되면서 메뉴가 수정
+	 * then: 수정된 메뉴의 이름과 가격이 DTO에 명시된 값과 같은지 검증
+	 */
 	@Test
 	@DisplayName("메뉴 수정 성공")
 	public void updateMenuTest() {
@@ -81,6 +92,7 @@ class MenuServiceTest {
 		when(storeService.findStoreById(storeId)).thenReturn(store);
 		when(menuRepository.findById(menuId)).thenReturn(Optional.of(menu));
 
+		// Mock 객체의 상태 변경을 설정
 		doAnswer(invocation -> {
 			String menuName = invocation.getArgument(0);
 			int price = invocation.getArgument(1);
@@ -97,7 +109,12 @@ class MenuServiceTest {
 		assertThat(responseDto.price()).isEqualTo(20000);
 	}
 
-
+	/**
+	 * 메뉴 삭제 성공 테스트
+	 * given: 삭제 대상 메뉴와 관련된 Mock 객체 설정. 메뉴 삭제 여부를 확인하기 위해 Mock 설정에서 menu.isDeleted()를 true로 반환하도록 설정
+	 * when: deleteMenu 메서드 실행. Mock 설정에 따라 storeService.findStoreById, menuRepository.findById 이 호출되면서 메뉴 삭제
+	 * then: 삭제된 메뉴의 ID와 삭제 상태(isDeleted)가 기댓값과 같은지 검증
+	 */
 	@Test
 	@DisplayName("메뉴 삭제 성공")
 	public void deleteMenuTest() {
