a95c80bda1a48b38167c190834e5bb17d4af690e
sinwoo-kim <shinwoo5676@gmail.com>
2025-01-11 20:54:46 +0900

    Feat: email 유니크 누락

--- src/main/java/com/example/outsourcingproject/domain/user/entity/User.java
+++ src/main/java/com/example/outsourcingproject/domain/user/entity/User.java
@@ -36,7 +36,7 @@ public class User {
 	@Column(nullable = false)
 	private String nickName;
 
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String email;
 
 	@Column(nullable = false)
